fa7741668046b406adbdeeecc96c33fe7668a9a6
mrfidget <ferdie@ferdie.co.uk>
2020-04-01 09:43:33 +0100

    first commit

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    private float translateSpeed = 0.1f;
+    public Vector3 upperYBound = new Vector3(0,1.3f,0);
+    public Vector3 lowerYBound = new Vector3(0, 0.1f, 0);
+    private Vector3 moveDirection = Vector3.zero;
+
+    private short flipYtransform = 1;
+
+    GameObject lava;
+    
+    // Start is called before the first frame update
+    void Start()
+    {        
+        lava = this.gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveLava();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        if(player != null)
+        {
+            player.isDead = true;
+        }
+    }
+
+    void moveLava()
+    {
+        if ((lava.transform.position.y >= upperYBound.y) || (lava.transform.position.y <= lowerYBound.y))
+        {
+            flipYtransform *= -1;
+        }
+        moveDirection = flipYtransform * new Vector3(0, (lava.transform.position.y + translateSpeed) * Time.deltaTime, 0);
+        lava.transform.position = lava.transform.position + moveDirection;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    GameObject player;
+    CharacterController characterController;
+    
+    public float translateSpeed = 2.0f;
+    public float rotateSpeed = 3.5f;
+    private float jumpHeight = 10.0f;
+    private float gravity = 20.0f;
+    
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+
+    public bool isDead;
+    private int currentHealth = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.gameObject;
+        characterController = player.GetComponent<CharacterController>();
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checkDeathStatus();
+        if (!isDead) {
+            movePlayer();
+        }
+        
+    }
+    private void checkDeathStatus()
+    {
+        if (isDead) { return; }
+        //if(currentHealth == 0)
+        //{
+        //    isDead = true;
+        //    //player.SetActive(false);
+        //    //GetComponent<FirstPersonController>().enabled = false;
+        //}
+
+    }
+    private void movePlayer()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            moveDirection = player.transform.TransformDirection(moveDirection);
+            moveDirection *= translateSpeed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpHeight;
+            }
+        }
+        else
+        {
+            //Debug.Log("not grounded");
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotateSpeed * Time.deltaTime, 0);
+        rotation = transform.TransformDirection(rotation);
+        player.transform.Rotate(rotation);
+
+    }
+}
